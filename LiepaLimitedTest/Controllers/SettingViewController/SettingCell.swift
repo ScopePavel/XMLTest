07b40b65da29dcf32c89ac85482f0411e1a493dc
pavelscope <p.sc4pe@mail.ru>
2023-08-04 19:46:00 +0700

    lint

--- LiepaLimitedTest/Controllers/SettingViewController/SettingCell.swift
+++ LiepaLimitedTest/Controllers/SettingViewController/SettingCell.swift
@@ -27,8 +27,9 @@ final class SettingCell: UITableViewCell, ReusableView {
         setup()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        preconditionFailure("init?(coder:) has not been implemented")
     }
 
     func config(parser: ParserProtocol) {
